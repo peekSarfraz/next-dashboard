06dec7d9f268bee7ac55a9e5562cda4235723594
toseef31 <peekinternational@gmail.com>
2025-07-09 17:51:52 +0500

    Inter font issue is fixed

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,4 +1,4 @@
-import { Inter, inter } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 // export const inter = inter({ weight: "400" });
